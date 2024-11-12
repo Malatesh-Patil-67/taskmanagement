570c5cfd12523ef93f4953c2a7a969146389c696
Malatesh-Patil-67 <malateshpatil6767@example.com>
2024-11-12 03:41:52 +0100

    added task model

---
+++ src/main/java/com/celonis/tasks/tasks/model/Task.java
@@ -0,0 +1,29 @@
+package com.celonis.tasks.tasks.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String status;
+
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    // Getters and Setters
+    // to be rendered by lombok.
+}
